5d6e494f7b55657c39973bab94fe49b45f00c6d9
Aksu <79048696+xiashanfengjian@users.noreply.github.com>
2023-02-28 14:32:16 +0800

    Update getgooglescholar3.py

--- getgooglescholar3.py
+++ getgooglescholar3.py
@@ -11,7 +11,7 @@ def content(key,time1,time2,m):
         w2 = 'https://xs.zidianzhan.net/scholar?start='+str((j-1)*10)+'&q='+key+'&hl=zh-CN&as_sdt=0,5&as_ylo='+time1+'&as_yhi='+time2
         url = w2
         urlget = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41',
-                                            'Cookie': '__gads=ID=399d8003ccf39920-229d8b4996d900d3:T=1675824466:RT=1675824466:S=ALNI_MZ-GXEu8yhlnFO5OBRmKL59cRb4MQ; Hm_lvt_f8f862c87446f2920a0aaf66facaf11f=1675824466,1676004278,1676427446; __gpi=UID=00000bb7a8c72cd8:T=1675824466:RT=1676427445:S=ALNI_MbNfZNNKvA6v4cm63V4nlErbs9jfA; security_session_verify=269bc72310145130b60dff5e2a889c83; security_session_mid_verify=a04be94e0b878c3082d8caddc5dffca3; Hm_lpvt_f8f862c87446f2920a0aaf66facaf11f=1676446387'})
+                                            'Cookie': 'xxxx'})
         urlget.encoding = urlget.apparent_encoding
         xp1 = etree.HTML(urlget.text)
         id_paper = xp1.xpath("//h3[@class='gs_rt']/a/@id")
